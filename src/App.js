08286db3bbedede6242bd519ad05d7e2dfd89c5f
pashbylogan <pashbylogan@gmail.com>
2023-02-12 21:55:27 +0000

    trying to fix pathing in static site on render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Bloglist from "./pages/Bloglist";
 import BlogDetails from "./pages/BlogDetails";
@@ -8,14 +8,14 @@ import "./App.scss";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
+    <HashRouter>
+      <div>
+        <Route exact path="/" element={<Homepage />} />
         <Route path="/blogs" component={Bloglist} exact />
         <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
         <Route path="/fictionary" element={<Fictionary />} />
-      </Routes>
-    </BrowserRouter>
+      </div>
+    </HashRouter>
   );
 }
 
