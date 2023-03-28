1ebb0125bbfc15c6185d5e18e8af7fe3b1520643
Logan Pashby <pashbylogan@gmail.com>
2023-03-28 11:17:24 -0400

    building correctly now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Bloglist from "./pages/Bloglist";
 import BlogDetails from "./pages/BlogDetails";
@@ -8,14 +8,16 @@ import "./App.scss";
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <div>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="/blogs" component={Bloglist} exact />
-        <Route path="/blogs/blog-details/:id/:title" component={BlogDetails} />
-        <Route path="/fictionary" element={<Fictionary />} />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/blogs" element={<Bloglist />} />
+          <Route path="/blogs/blog-details/:id/:title" element={<BlogDetails />} />
+          <Route path="/fictionary" element={<Fictionary />} />
+        </Routes>
       </div>
-    </HashRouter>
+    </Router>
   );
 }
 
